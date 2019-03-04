9c1eeb5e96f1db39e6b070dcc8dff8900c7cc5a7
Robin Cheptileh <robincheptileh@gmail.com>
2019-03-04 16:56:59 +0300

    add samples and data checks

--- simple-firestore-import.py
+++ simple-firestore-import.py
@@ -11,33 +11,60 @@ from firebase_admin import firestore
 
 
 def import_data(service_account_key_path, data_file, collection_name):
-    allowed_file_extensions = ["json", "yaml", "yml"]
-
     try:
         cred = credentials.Certificate(service_account_key_path)
         firebase_admin.initialize_app(cred)
 
         db = firestore.client()
 
-        filename, file_extension = os.path.splitext(data_file)
-
-        if file_extension.lower in allowed_file_extensions:
-            pass
-        else:
-            raise ValueError("Invalid file type. Allowed file types include {}".format(allowed_file_extensions))
+        data = get_data(data_file)
+        check_data(data)
 
     except Exception as error:
         print(str(error))
+
+
+def check_data(data):
+    if isinstance(data, (list, tuple,)):
+        for datum in data:
+            if not isinstance(datum, (object,)):
+                raise ValueError("An object expected, got {}".format(type(datum)))
+    else:
+        raise ValueError("An array expected, got {}".format(type(data)))
+
+
+def get_data(data_file):
+    allowed_file_extensions = [".json", ".yaml", ".yml"]
+    filename, file_extension = os.path.splitext(data_file)
+    file_extension = file_extension.lower()
+
+    if file_extension in allowed_file_extensions:
+        if file_extension == ".json":
+            return get_json_data(data_file)
+        elif file_extension == ".yaml" or file_extension == ".yml":
+            return get_yaml_data(data_file)
+
+    raise ValueError("Invalid file type. Allowed file types include {}".format(allowed_file_extensions))
+
+
+def get_json_data(data_file):
+    with open(data_file, "r") as read_file:
+        return json.load(read_file)
+
+
+def get_yaml_data(data_file):
+    with open(data_file, "r") as read_file:
+        return yaml.load(read_file, Loader=yaml.FullLoader)
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
         service_account_path = sys.argv[1]
         data_file_path = sys.argv[2]
-        collection_name = sys.argv[3]
+        name_of_collection = sys.argv[3]
     else:
         service_account_path = input("Path to serviceAccountKey.json: ")
         data_file_path = input("Path to data file: ")
-        collection_name = input("Name of collection: ")
+        name_of_collection = input("Name of collection: ")
 
-    import_data(service_account_path, data_file_path, collection_name)
+    import_data(service_account_path, data_file_path, name_of_collection)
